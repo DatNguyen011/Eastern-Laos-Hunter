35b75486d649c3aa04c2b093ae9983480081a1f0
Dat Nguyen <earsdat@gmail.com>
2024-10-24 00:54:39 +0700

    Move AWDS - Coutertime

--- Eastern Laos Hunter/Assets/Scripts/Hero.cs
+++ Eastern Laos Hunter/Assets/Scripts/Hero.cs
@@ -4,38 +4,38 @@ using UnityEngine;
 
 public class Hero : Singleton<Hero>
 {
-    
-    [SerializeField] float speed=5f;
-    private bool facingRight=true;
+
+    [SerializeField] float speed = 5f;
+    private bool facingRight = true;
     public Animator animator;
     private string currentAnim;
-    private bool isAttack=false;
-    public float timeToDash = 5f;
-    public float dashTime = 1f;
-    
+    private bool isAttack = false;
+    public float countDownDash = 5f;
+    public float dashTime = 0.3f;
+    public CounterTime couterTime=new CounterTime();
     void Start()
     {
-        
+
     }
 
     //Joystick
     //void Update()
     //{       
     //    transform.position += JoystickControl.direct*speed*Time.deltaTime;
-        
+
     //    if (JoystickControl.direct.x>0&& !facingRight)
     //    {
     //        facingRight = !facingRight;
     //        transform.Rotate(0, 180f, 0);
     //        ChangeAnim("Run");
-            
+
     //    }
     //    else if (JoystickControl.direct.x < 0&&facingRight)
     //    {
     //        facingRight = !facingRight;
     //        transform.Rotate(0, 180f, 0);
     //        ChangeAnim("Run");
-            
+
     //    }
     //    else if (JoystickControl.direct == Vector3.zero && !isAttack)
     //    {
@@ -46,55 +46,79 @@ public class Hero : Singleton<Hero>
     //AWDS
     void Update()
     {
-
-        float moveX = Input.GetAxis("Horizontal");
-        float moveY = Input.GetAxis("Vertical");
-        Vector3 move=new Vector3 (moveX, moveY, 0)*speed*Time.deltaTime;
-        transform.position += move;
-
-        if (move.x > 0 || move.y > 0 && !facingRight)
+        if (isAttack == false)
         {
-            facingRight = !facingRight;
-            transform.Rotate(0, 180f, 0);
-            ChangeAnim("Run");
+            float moveX = Input.GetAxis("Horizontal");
+            float moveY = Input.GetAxis("Vertical");
+            Vector3 move = new Vector3(moveX, moveY, 0).normalized;
+            transform.position += move * speed * Time.deltaTime;
+            if(move == Vector3.zero)
+            {
+                ChangeAnim("Idle");
+            }
+            
+            if (move != Vector3.zero)
+            {
+                ChangeAnim("Run");
+                if (move.x > 0 && !facingRight)
+                {
+                    facingRight = !facingRight;
+                    transform.Rotate(0, 180f, 0);
+                }
+                else if (move.x < 0 && facingRight)
+                {
+                    facingRight = !facingRight;
+                    transform.Rotate(0, 180f, 0);
+                }
+            }
+            if(Input.GetKeyDown(KeyCode.J))
+            {
+                Attack();
+            }
+            else if (Input.GetKeyDown(KeyCode.K))
+            {
+                Dash();
+            }
 
         }
-        else if (move.x < 0 || move.y < 0 && facingRight)
-        {
-            facingRight = !facingRight;
-            transform.Rotate(0, 180f, 0);
-            ChangeAnim("Run");
 
-        }
-        else if (move == Vector3.zero && !isAttack)
-        {
-            ChangeAnim("Idle");
-        }
     }
 
     public void Attack()
     {
-        ChangeAnim("Attack");
-        isAttack = true;
-        StartCoroutine(ReturnIdle());
+        if (isAttack == false)
+        {
+            ChangeAnim("Attack");
+            isAttack = true;
+            StartCoroutine(ReturnIdle());
+        }
+
     }
 
     IEnumerator ReturnIdle()
     {
         yield return new WaitForSeconds(.5f);
-        isAttack=false;
+        isAttack = false;
     }
 
     public void Dash()
     {
-        speed = speed+5f;
+        speed = speed + 5f;
+        UIManager.Instance.btnFlash.interactable = false;
+        StartCoroutine(Dashing());
         StartCoroutine(CountdownDashTime());
+    }
+
+    IEnumerator Dashing()
+    {
+        yield return new WaitForSeconds(dashTime);
+        speed = 5f;
     }
 
     IEnumerator CountdownDashTime()
     {
-        yield return new WaitForSeconds(dashTime);
-        speed = 5f;
+        yield return new WaitForSeconds(countDownDash);
+        UIManager.Instance.btnFlash.interactable = true;
     }
 
     public void ChangeAnim(string animName)
