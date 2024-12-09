8d20fee39503758bf1821a18dbab7a9e5e114066
Dat Nguyen <earsdat@gmail.com>
2024-12-10 01:52:35 +0700

    Dialog System

---
+++ Eastern Laos Hunter/Assets/Scripts/Dialogue System/DialogUI.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DialogUI : MonoBehaviour
+{
+    public TMP_Text textLabel;
+    void Start()
+    {
+        TypeWirteEffect.Instance.Run("ahfdadhjashcvuefjsssssssssssssssssssssssdfhweifh\nsjkhhkfss", textLabel);
+        
+    }
+
+}
